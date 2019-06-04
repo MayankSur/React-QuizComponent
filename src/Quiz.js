86cf503856785fe3c643136f62f4f99061c663a3
Mayank Surana <mayank.surana@oliverwyman.com>
2019-06-04 15:14:27 +0100

    TTask 8

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,10 @@ class Quiz extends Component {
 
   }
 
+  handleResetClick(){
+    this.setState({quiz_position : 1})
+  }
+
   showNextQuestion(){
     this.setState((state) => {
       return {quiz_position: state.quiz_position + 1}
@@ -22,7 +26,7 @@ class Quiz extends Component {
     const isQuizEnd = ((quizData.quiz_questions.length) === (this.state.quiz_position-1))
     return(
     <div>
-    {isQuizEnd ? <QuizEnd /> :
+    {isQuizEnd ? <QuizEnd resetClickHandler = {this.handleResetClick.bind(this)} /> :
       <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
       }
     </div>
